73ff8cd072a090933043a7f661eadacd34ab54b7
Bartosz Maziarka <bartosz.maziarka@solveq.io>
2022-02-26 12:31:59 +0100

    add footer contact link

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom'
+import { route } from '@/app/router/urls/routes'
+import { useTranslation } from 'react-i18next'
+
 const Footer = () => {
+  const {t} = useTranslation();
   return (
-    <footer id="main-footer" className="max-w-screen-2xl px-4 md:px-8 mx-auto w-full">
+    <footer id="main-footer" className="max-w-screen-2xl px-4 md:px-8 mx-auto w-full mb-2">
       <div className="container mx-auto">
-        <div className="border-t text-gray-400 text-sm text-center py-8">
-          © 2021 -  UAPomoc.pl
+        <div className="text-gray-500 border-t text-center py-2">
+          <Link
+            to={route['contact']}>
+            {t("header.contact")}
+          </Link>
+        </div>
+        <div className="text-gray-400 text-sm text-center">
+          © 2021 - UAPomoc.pl
         </div>
       </div>
     </footer>
