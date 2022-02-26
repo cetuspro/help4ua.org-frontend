5e46177793cbbd4fbc12a99821326cf34494f3ec
grze2000 <programista333@gmail.com>
2022-02-26 18:02:39 +0100

    show different info depending on notice type

--- src/views/notices/View_Notice/View_Notice.jsx
+++ src/views/notices/View_Notice/View_Notice.jsx
@@ -4,7 +4,19 @@ import { route } from '@/app/router/urls/routes'
 import { Breadcrumb } from '@/components/common/Breadcrumb'
 import { useParams } from 'react-router-dom'
 import { useGetNotice } from '../../../app/CRUD/notices/getNotice'
-import NoticeCard from './cards/Card_Notice'
+import HelpOfferCard from './cards/Card_HelpOffer'
+import ShelterOfferCard from './cards/Card_ShelterOffer'
+import ShelterSearchCard from './cards/Card_ShelterSearch'
+import TransportOfferCard from './cards/Card_transportOffer'
+import TransportSearchCard from './cards/Card_TransportSearch'
+
+const noticeTypes = {
+  1: ShelterOfferCard,
+  10: ShelterSearchCard,
+  20: TransportOfferCard,
+  22: TransportSearchCard,
+  50: HelpOfferCard,
+}
 
 const breadcrumbItems = (title) => [
   {
@@ -19,6 +31,7 @@ const breadcrumbItems = (title) => [
 const ViewNotice = () => {
   const { noticeId } = useParams()
   const query = useGetNotice(noticeId)
+  const CardComponent = query?.data?.type in noticeTypes ? noticeTypes[query?.data?.type] : noticeTypes[1]
 
   return (
     <>
@@ -28,7 +41,7 @@ const ViewNotice = () => {
             <>
               <Breadcrumb items={breadcrumbItems(query?.data?.address)}/>
               <div className="grid gap-8">
-                <NoticeCard/>
+                <CardComponent/>
               </div>
             </>
           )}
---
+++ src/views/notices/View_Notice/cards/Card_transportOffer.jsx
@@ -0,0 +1,51 @@
+import { useQueryContext } from '@/app/context/queries/QueryProvider'
+import { route } from '@/app/router/urls/routes'
+import Card from '@/components/common/Card'
+import Button from '@/components/common/Button'
+import dayjs from 'dayjs'
+import { getPeriod, getValue } from '@/views/notices/View_Notices/dataTable/DataTable_Notices'
+import { useTranslation } from 'react-i18next'
+import { voivodeshipsEnum } from '@/app/config/enum/voivodeships'
+
+const Item = ({label, value}) => {
+  return (
+    <div className="py-2 flex gap-2">
+      <span className="">{label}</span>
+      <span className="font-bold">{value}</span>
+    </div>
+  )
+}
+
+const TransportOfferCard = () => {
+  const { data: {
+    description,
+    cityName,
+    region,
+    address,
+    location,
+    id,
+    name,
+    phoneNumber,
+    createdAt,
+  } } = useQueryContext()
+  const { t } = useTranslation();
+  const getRegion = val => voivodeshipsEnum(t).find(item => item.value === val)?.label ?? "Brak danych";
+  const href = location?.lat && location?.long ? `http://www.google.com/maps/place/${location?.lat},${location?.long}` : `https://www.google.com/maps/search/${cityName??''}+${getRegion(region)??''}+${address??''}`
+
+  return (
+    <Card>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-1">
+          {!!description && <Item label="Opis:" value={description}/>}
+          {!!name && <Item label="Imię:" value={name}/>}
+          {!! phoneNumber &&<Item label="Telefon:" value={phoneNumber}/>}
+          {!! createdAt &&<Item label="Data dodania:" value={dayjs(createdAt).format('DD.MM.YYYY HH:mm')}/>}
+          {!! id &&<Item label="Identyfikator:" value={id}/>}
+        </div>
+      </div>
+      <Button to={route['notices.list4']} className="mt-10 mx-auto w-fit" size="small">Wróć do listy ogłoszeń</Button>
+    </Card>
+  )
+}
+
+export default TransportOfferCard
