f34e0aad8f31f963f771f075ee4d1c5c2018302d
Bartosz Maziarka <bartosz.maziarka@solveq.io>
2022-02-27 16:21:48 +0100

    add responsiveness to the contact page

--- src/views/public/contact/Contact.jsx
+++ src/views/public/contact/Contact.jsx
@@ -4,15 +4,15 @@ export default function Contact() {
   const {t} = useTranslation();
   return (
     <>
-      <div className="items-center p-8 mx-20 flex justify-center">
-        <div className="flex-1 justify-end p-4 flex">
+      <div className="items-center p-8 mx-auto md:mx-20 md:flex justify-center">
+        <div className="flex-1 p-4 flex-start md:justify-end flex">
           <iframe
             src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fuapomocpl%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
             width="360" height="500" className="border-none overflow-hidden" scrolling="no" frameBorder="0"
             allowFullScreen="true"
             allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
-        <div className="flex-1 p-4 h-full">
+        <div className="flex-1 p-4 h-full md:flex md:text-center">
           <h1>{t("header.contact")}:</h1>
           <h2>akcjauapomoc@gmail.com</h2>
         </div>
