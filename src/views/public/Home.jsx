614b14f23dfe9407617558ec530d4896fa3e5857
Rafal <rafii2000@interia.pl>
2022-02-28 16:53:43 +0100

    fix: landing page button scroll to wrong section

--- src/views/public/Home.jsx
+++ src/views/public/Home.jsx
@@ -26,7 +26,7 @@ export default function Home() {
 
                 <div className="flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5">
                   <a
-                    href="#features"
+                    href="#zgloszenia"
                     className="inline-block bg-yellow-400 hover:bg-yellow-600 active:bg-yellow-700 focus-visible:ring ring-yellow-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">
                     {t('header.notices')}
                   </a>
@@ -46,7 +46,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div id="features" className="bg-gray-100 dark:bg-gray-900 pb-6 sm:pb-8 lg:pb-12">
+      <div className="bg-gray-100 dark:bg-gray-900 pb-6 sm:pb-8 lg:pb-12">
         <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
           <div className="mb-10 md:mb-16">
             <p className="max-w-screen-md text-gray-600 mb-2 text-xl text-center mx-auto ">
@@ -66,7 +66,9 @@ export default function Home() {
             </p>
           </div>
           {/* FEATURES */}
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-6 xl:gap-16 md:px-10 py-2">
+          <div
+            id="zgloszenia"
+            className="grid sm:grid-cols-1 md:grid-cols-2 gap-6 xl:gap-16 md:px-10 py-2">
             <div className="flex flex-col items-center justify-center p-8 shadow-md rounded-3xl">
               <div className="w-16 h-16 md:h-24 md:w-24 flex justify-center items-center text-yellow-400 mb-2 sm:mb-4">
                 <FiHome size={100} color="currentColor" />
@@ -202,7 +204,7 @@ export default function Home() {
                 {t('formDescription.offerHelp')}
               </p>
             </div>
-            
+
             <div className="flex flex-col items-center justify-center p-8 shadow-md rounded-3xl">
               <div className="w-16 h-16 md:h-24 md:w-24 flex justify-center items-center text-yellow-400 mb-2 sm:mb-4">
                 <FiGift size={100} color="currentColor" />
@@ -213,7 +215,8 @@ export default function Home() {
               </h3>
 
               <p className={'italic mb-4'}>
-                {t('frontpage.activeAds')}: {!isFetching && (data?.[7]?.statusAndAmount?.[2]?.amount ?? '-')}
+                {t('frontpage.activeAds')}:{' '}
+                {!isFetching && (data?.[7]?.statusAndAmount?.[2]?.amount ?? '-')}
               </p>
               <div className="flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5">
                 <Button1 to={route['notices.addFindHelp']} label={t('frontpage.addNotice')} />
@@ -239,7 +242,8 @@ export default function Home() {
               </h3>
 
               <p className={'italic mb-4'}>
-                {t('frontpage.activeAds')}: {!isFetching && (data?.[6]?.statusAndAmount?.[2]?.amount ?? '-')}
+                {t('frontpage.activeAds')}:{' '}
+                {!isFetching && (data?.[6]?.statusAndAmount?.[2]?.amount ?? '-')}
               </p>
               <div className="flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5">
                 <Button1
@@ -257,7 +261,7 @@ export default function Home() {
                 {t('formDescription.translations')}
               </p>
             </div>
-            
+
             <div className="flex flex-col items-center justify-center p-8 shadow-md rounded-3xl">
               <div className="w-16 h-16 md:h-24 md:w-24 flex justify-center items-center text-yellow-400 mb-2 sm:mb-4">
                 <FiEdit size={100} color="currentColor" />
@@ -268,7 +272,8 @@ export default function Home() {
               </h3>
 
               <p className={'italic mb-4'}>
-                {t('frontpage.activeAds')}: {!isFetching && (data?.[8]?.statusAndAmount?.[2]?.amount ?? '-')}
+                {t('frontpage.activeAds')}:{' '}
+                {!isFetching && (data?.[8]?.statusAndAmount?.[2]?.amount ?? '-')}
               </p>
               <div className="flex flex-col sm:flex-row sm:justify-center lg:justify-start gap-2.5">
                 <Button1
@@ -286,7 +291,7 @@ export default function Home() {
                 {t('formDescription.lfTranslations')}
               </p>
             </div>
-            
+
             <div className="flex flex-col items-center justify-center p-8 shadow-md rounded-3xl">
               <div className="w-16 h-16 md:h-24 md:w-24 flex justify-center items-center text-yellow-400 mb-2 sm:mb-4">
                 <FaMapMarkerAlt size={100} color="currentColor" />
