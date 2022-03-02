79d79e7e27e30f7b25ae30f4fdb6e3f29ee3b3a0
=grze2000 <programista333@gmail.com>
2022-03-02 14:23:56 +0100

    fixes for help points map

--- src/app/CRUD/helpPoints/getHelpPoints.jsx
+++ src/app/CRUD/helpPoints/getHelpPoints.jsx
@@ -2,18 +2,19 @@ import axios from 'axios'
 import { API_URL } from '../../config/env'
 import { usePaginatedQuery2 } from '@/app/hooks/usePaginatedQuery2'
 
-const getHelpPoints = (key, pagination) =>
+const getHelpPoints = (params) => (key, pagination) =>
   axios({
     method: 'GET',
     url: `${API_URL}/help-points`,
     params: {
       ...pagination,
+      ...params
     },
   }).then(({ data }) => data)
 
-export const useGetHelpPoints = () =>
+export const useGetHelpPoints = (params) =>
   usePaginatedQuery2({
     queryKey: ['app.helpPoints'],
-    queryFn: getHelpPoints,
+    queryFn: getHelpPoints(params),
     defaultPageSize: 50,
   })
