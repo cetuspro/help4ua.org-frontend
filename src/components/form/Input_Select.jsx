4a407c423035a67c333bfb8613002d2e6faabbc8
Maciej Kaminski <mkam@ciklum.com>
2022-03-03 22:13:14 +0100

    Add country selection to forms

--- src/components/form/Input_Select.jsx
+++ src/components/form/Input_Select.jsx
@@ -1,18 +1,26 @@
+import { useEffect} from 'react'
 import { InputSelectPure } from './Input_Select.pure'
 import { Controller, useFormContext } from 'react-hook-form'
 
 export const InputSelect = ({
   options,
-  disabled,
+  disabled = false,
   name,
   placeholder,
   label,
   isClearable,
   required,
   optionFormat,
+  hidden = false,
+  onChange: handleChange,
   ...props
 }) => {
   const { control } = useFormContext()
+
+
+useEffect(() => {
+  disabled ? control.unregister(name) : control.register(name)
+},[disabled])
 
   return (
     <Controller
@@ -20,12 +28,13 @@ export const InputSelect = ({
       control={control}
       render={({ field: { ref, value, onChange, ...field }, fieldState }) => {
         const { error } = fieldState
+       
         return (
           <InputSelectPure
             innerRef={ref}
-            onChange={(data) => onChange(data?.value ?? null)}
+            onChange={(data) => {handleChange && handleChange(data); onChange(data?.value ?? null)}}
             isClearable={isClearable ?? !required}
-            {...{ options, value, disabled, placeholder, label, error, required, optionFormat }}
+            {...{ options, value, disabled, placeholder, label, error, required, optionFormat, hidden }}
             {...props}
             {...field}
           />
