5d9d19bf0091424a0a28ebbdc8d454bd45159e57
myy <myy@ciklum.com>
2022-03-15 11:48:02 +0100

    fix offer description

--- src/views/notices/components/common/Fields/Description.jsx
+++ src/views/notices/components/common/Fields/Description.jsx
@@ -9,29 +9,35 @@ const Description = ({ descriptionPL, descriptionUA, descriptionEN }) => {
 
   return (
     <>
-      <NoticeDetailsItem
-        label={descriptionPL && t('common.opis')}
-        className="flex-col"
-        icon={<Icon.MdOutlineTextSnippet {...iconConfig} />}
-        value={descriptionPL}
-        valueClassName={`text-gray-800 font-normal `}
-      />
-
-      <NoticeDetailsItem
-        label={descriptionUA && t('common.opisUA')}
-        className="flex-col"
-        icon={<Icon.MdOutlineTextSnippet {...iconConfig} />}
-        value={descriptionUA}
-        valueClassName={`text-gray-800 font-normal `}
-      />
-
-      <NoticeDetailsItem
-        label={descriptionEN && t('common.opisEN')}
-        className="flex-col"
-        icon={<Icon.MdOutlineTextSnippet {...iconConfig} />}
-        value={descriptionEN}
-        valueClassName={`text-gray-800 font-normal `}
-      />
+      {descriptionPL &&
+        <NoticeDetailsItem
+          label={descriptionPL && t('common.opis')}
+          className="flex-col"
+          icon={<Icon.MdOutlineTextSnippet {...iconConfig} />}
+          value={descriptionPL}
+          valueClassName={`text-gray-800 font-normal `}
+        />
+      }
+      {
+        descriptionUA &&
+          <NoticeDetailsItem
+            label={descriptionUA && t('common.opisUA')}
+            className="flex-col"
+            icon={<Icon.MdOutlineTextSnippet {...iconConfig} />}
+            value={descriptionUA}
+            valueClassName={`text-gray-800 font-normal `}
+          />
+      }
+      {
+        descriptionEN &&
+          <NoticeDetailsItem
+            label={descriptionEN && t('common.opisEN')}
+            className="flex-col"
+            icon={<Icon.MdOutlineTextSnippet {...iconConfig} />}
+            value={descriptionEN}
+            valueClassName={`text-gray-800 font-normal `}
+          />
+      }
     </>
   )
 }
